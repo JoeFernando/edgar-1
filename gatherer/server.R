294e0ba8f1f16767d2e4d252fc15d7551cdcf640
shelmich <sam.helmich@gmail.com>
2014-03-27 09:52:38 -0500

    Added a filings lookup tab to the gatherer app

--- gatherer/server.R
+++ gatherer/server.R
@@ -1,9 +1,18 @@
 source('useful functions.R')
 shinyServer(function(input, output){
+  
+  
   output$ciktable <- renderTable({
     result <- cik.lookup(input$toSearch)
     if(!identical(NULL, result)){
       result
+    }
+  })
+  
+  output$filingstable <- renderTable({
+    result <- get.filing.locs(cik = as.numeric(input$cik), type = input$formtype)
+    if(!identical(NULL, result)){
+      result[,c("Filings", "Filing Date", "File/Film Number")]
     }
   })
 })
--- gatherer/ui.R
+++ gatherer/ui.R
@@ -1,10 +1,28 @@
 shinyUI(pageWithSidebar(
   headerPanel("Financial Data Gatherer"),
   
-  sidebarPanel(textInput(inputId = "toSearch",
-            label = "CIK Lookup",
-            value = "Enter search term...")),
+  sidebarPanel(
+    wellPanel(
+      textInput(inputId = "toSearch",
+                label = "CIK Lookup",
+                value = "Google")
+      ),
+    
+    wellPanel(
+      textInput(inputId = "cik",
+                   label = "Filings Search",
+                   value = "0001288776"),
+      
+      textInput(inputId = "formtype",
+                label = "Form Type",
+                value = "10-K")
+      )
+  ),
   
-  mainPanel(tableOutput(outputId = "ciktable"))
-  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("CIK Lookup", tableOutput(outputId = "ciktable")),
+      tabPanel("Filings Lookup", tableOutput(outputId = "filingstable"))
+      )
+    )
   ))
